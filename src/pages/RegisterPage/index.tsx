529cb3e8b0eba9d376610fb7568c55a784d31e11
Almas Kilichov <itsmywaytoamerica@gmail.com>
2023-12-11 13:54:12 +0600

    feat: add form to register page and implement validation

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,7 +1,61 @@
-import styles from './RegisterPage.module.scss'
+// import styles from './RegisterPage.module.scss';
+import { object, ref, string } from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { User } from '../../entities/user.interface';
+
+const schema = object().shape({
+  email: string()
+    .email('Enter valid email!')
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      'Enter valid email!',
+    )
+    .required('Enter email!'),
+  password: string()
+    .matches(
+      /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]).{8,}$/,
+      'Your password are weak!',
+    )
+    .required("Enter password!"),
+  ['confirm-password']: string()
+    .oneOf([ref('password')], 'Passwords must match')
+    .required("Passwords must match"),
+});
 
 function RegisterPage() {
-  return <h1 className={styles.titleTest}>Страница Регистрации</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function onRegister(data: User) {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onRegister)}>
+      <label htmlFor="email">Email:</label>
+      <input type="email" {...register('email')} id="email" />
+      {errors.email && <p>{errors.email.message}</p>}
+      <label htmlFor="password">Password:</label>
+      <input type="password" {...register('password')} id="password" />
+      {errors.password && <p>{errors.password.message}</p>}
+      <label htmlFor="confirm-password">Confirm Password:</label>
+      <input
+        type="password"
+        {...register('confirm-password')}
+        id="confirm-password"
+      />
+      {errors['confirm-password'] && (
+        <p>{errors['confirm-password'].message}</p>
+      )}
+      <input type="submit" value="Register" />
+    </form>
+  );
 }
 
 export default RegisterPage;
