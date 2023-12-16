83dbc0c2257f39b9f715cc4098f26c88da73212b
Almas Kilichov <itsmywaytoamerica@gmail.com>
2023-12-16 12:23:38 +0600

    chore: make route graphiql private

--- src/pages/GraphiQLPage/index.tsx
+++ src/pages/GraphiQLPage/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import styles from './GraphiQLPage.module.scss'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import { auth } from '../../firebase/firebase';
+import { useNavigate } from 'react-router-dom';
 
 function GraphiQLPage() {
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) navigate('/login')
+  })
+
   return <h1 className={styles.titleTest}>Страница GraphiQL</h1>;
 }
 
