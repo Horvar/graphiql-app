876016ad976b74e1f10175a4c5232cdcfcb287f9
Калашников Семен <horselvaren@gmail.com>
2023-12-20 00:24:35 +0600

    feat: add styles for Error Boundary

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -8,16 +8,17 @@ interface ErrorBoundaryProps {
 
 interface ErrorBoundaryState {
   hasError: boolean;
+  error: Error | null;
 }
 
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: null };
   }
 
-  static getDerivedStateFromError(): ErrorBoundaryState {
-    return { hasError: true };
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error: error };
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
@@ -27,7 +28,26 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   render() {
     if (this.state.hasError) {
       return (
-        <h1 className={styles.titleTest}>Произошла ошибка (Error Boundary)</h1>
+        <>
+          <section className={styles.errorBoundary}>
+            <div
+              className={`${styles.errorBoundaryContainer} container container--small`}
+            >
+              <h1 className={`${styles.errorBoundaryTitle} title-1`}>
+                Unexpected Error Encountered
+              </h1>
+              <div className={`${styles.errorBoundaryText} text-common`}>
+                {this.state.error?.message}
+              </div>
+              <button
+                className={styles.errorBoundaryButton}
+                onClick={() => window.location.reload()}
+              >
+                Reload
+              </button>
+            </div>
+          </section>
+        </>
       );
     }
 
