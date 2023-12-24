ef8b9d139b66d8ec0f5257d4f9e6fd6cfe07d4ca
Калашников Семен <horselvaren@gmail.com>
2023-12-24 21:36:27 +0600

    test: add test for NotFoundPage

---
+++ tests/NotFoundPage.test.tsx
@@ -0,0 +1,22 @@
+/** @jsxImportSource react */
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import NotFoundPage from '../src/pages/NotFoundPage';
+
+describe('NotFoundPage', () => {
+  it('renders correctly', () => {
+    render(
+      <Router>
+        <NotFoundPage />
+      </Router>,
+    );
+
+    expect(screen.getByText(/Page Not Found/i)).toBeInTheDocument();
+    expect(screen.getByText(/404/i)).toBeInTheDocument();
+
+    const homeLink = screen.getByRole('link', { name: /Homepage/i });
+    expect(homeLink).toBeInTheDocument();
+    expect(homeLink.getAttribute('href')).toBe('/');
+  });
+});
