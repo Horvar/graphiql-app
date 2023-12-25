586aed315235ee55f60b95fcd874e0f70972e0cc
Калашников Семен <horselvaren@gmail.com>
2023-12-25 20:25:35 +0600

    test: add tests for password-validation

--- src/__tests__/password-validation.test.tsx
+++ src/__tests__/password-validation.test.tsx
@@ -1,227 +1,45 @@
-import * as yup from 'yup';
+import { passwordSchema } from '../schemas/yup/password-validation';
 
-describe('Password Schema Validation', () => {
-  it('should pass with a valid password', async () => {
-    const validPassword = 'Password123!';
-
-    const isValid = await yup
-      .string()
-      .required()
-      .test(
-        'has-digit',
-        'Your password must contain at least one digit',
-        (value) => /\d/.test(value),
-      )
-      .test(
-        'has-uppercase',
-        'Your password must contain at least one uppercase letter',
-        (value) => /[A-Z]/.test(value),
-      )
-      .test(
-        'has-lowercase',
-        'Your password must contain at least one lowercase letter',
-        (value) => /[a-z]/.test(value),
-      )
-      .test(
-        'has-special-char',
-        'Your password must contain at least one special character',
-        (value) => /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(value),
-      )
-      .min(8, 'Your password must be at least 8 characters long')
-      .isValid(validPassword);
-
-    expect(isValid).toBe(true);
+describe('password validation', () => {
+  it('should require a password', async () => {
+    await expect(passwordSchema.validate('')).rejects.toThrow(
+      'Enter password!',
+    );
   });
 
-  it('should fail if password is empty', async () => {
-    const emptyPassword = '';
-
-    const isValid = await yup
-      .string()
-      .required()
-      .test(
-        'has-digit',
-        'Your password must contain at least one digit',
-        (value) => /\d/.test(value),
-      )
-      .test(
-        'has-uppercase',
-        'Your password must contain at least one uppercase letter',
-        (value) => /[A-Z]/.test(value),
-      )
-      .test(
-        'has-lowercase',
-        'Your password must contain at least one lowercase letter',
-        (value) => /[a-z]/.test(value),
-      )
-      .test(
-        'has-special-char',
-        'Your password must contain at least one special character',
-        (value) => /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(value),
-      )
-      .min(8, 'Your password must be at least 8 characters long')
-      .isValid(emptyPassword);
-
-    expect(isValid).toBe(false);
+  it('should require a digit', async () => {
+    await expect(passwordSchema.validate('Password')).rejects.toThrow(
+      'Your password must contain at least one digit',
+    );
   });
 
-  it('should fail if password has no digits', async () => {
-    const noDigitPassword = 'PasswordNoDigit';
-
-    const isValid = await yup
-      .string()
-      .required()
-      .test(
-        'has-digit',
-        'Your password must contain at least one digit',
-        (value) => /\d/.test(value),
-      )
-      .test(
-        'has-uppercase',
-        'Your password must contain at least one uppercase letter',
-        (value) => /[A-Z]/.test(value),
-      )
-      .test(
-        'has-lowercase',
-        'Your password must contain at least one lowercase letter',
-        (value) => /[a-z]/.test(value),
-      )
-      .test(
-        'has-special-char',
-        'Your password must contain at least one special character',
-        (value) => /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(value),
-      )
-      .min(8, 'Your password must be at least 8 characters long')
-      .isValid(noDigitPassword);
-
-    expect(isValid).toBe(false);
+  it('should require an uppercase letter', async () => {
+    await expect(passwordSchema.validate('password1')).rejects.toThrow(
+      'Your password must contain at least one uppercase letter',
+    );
   });
 
-  it('should fail if password has no uppercase letters', async () => {
-    const noUppercasePassword = 'password123!';
-
-    const isValid = await yup
-      .string()
-      .required()
-      .test(
-        'has-digit',
-        'Your password must contain at least one digit',
-        (value) => /\d/.test(value),
-      )
-      .test(
-        'has-uppercase',
-        'Your password must contain at least one uppercase letter',
-        (value) => /[A-Z]/.test(value),
-      )
-      .test(
-        'has-lowercase',
-        'Your password must contain at least one lowercase letter',
-        (value) => /[a-z]/.test(value),
-      )
-      .test(
-        'has-special-char',
-        'Your password must contain at least one special character',
-        (value) => /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(value),
-      )
-      .min(8, 'Your password must be at least 8 characters long')
-      .isValid(noUppercasePassword);
-
-    expect(isValid).toBe(false);
+  it('should require a lowercase letter', async () => {
+    await expect(passwordSchema.validate('PASSWORD1')).rejects.toThrow(
+      'Your password must contain at least one lowercase letter',
+    );
   });
 
-  it('should fail if password has no lowercase letters', async () => {
-    const noLowercasePassword = 'PASSWORD123!';
-
-    const isValid = await yup
-      .string()
-      .required()
-      .test(
-        'has-digit',
-        'Your password must contain at least one digit',
-        (value) => /\d/.test(value),
-      )
-      .test(
-        'has-uppercase',
-        'Your password must contain at least one uppercase letter',
-        (value) => /[A-Z]/.test(value),
-      )
-      .test(
-        'has-lowercase',
-        'Your password must contain at least one lowercase letter',
-        (value) => /[a-z]/.test(value),
-      )
-      .test(
-        'has-special-char',
-        'Your password must contain at least one special character',
-        (value) => /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(value),
-      )
-      .min(8, 'Your password must be at least 8 characters long')
-      .isValid(noLowercasePassword);
-
-    expect(isValid).toBe(false);
+  it('should require a special character', async () => {
+    await expect(passwordSchema.validate('Password1')).rejects.toThrow(
+      'Your password must contain at least one special character',
+    );
   });
 
-  it('should fail if password has no special characters', async () => {
-    const noSpecialCharPassword = 'Password123';
-
-    const isValid = await yup
-      .string()
-      .required()
-      .test(
-        'has-digit',
-        'Your password must contain at least one digit',
-        (value) => /\d/.test(value),
-      )
-      .test(
-        'has-uppercase',
-        'Your password must contain at least one uppercase letter',
-        (value) => /[A-Z]/.test(value),
-      )
-      .test(
-        'has-lowercase',
-        'Your password must contain at least one lowercase letter',
-        (value) => /[a-z]/.test(value),
-      )
-      .test(
-        'has-special-char',
-        'Your password must contain at least one special character',
-        (value) => /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(value),
-      )
-      .min(8, 'Your password must be at least 8 characters long')
-      .isValid(noSpecialCharPassword);
-
-    expect(isValid).toBe(false);
+  it('should require at least 8 characters', async () => {
+    await expect(passwordSchema.validate('Pass1!')).rejects.toThrow(
+      'Your password must be at least 8 characters long',
+    );
   });
 
-  it('should fail if password is less than 8 characters long', async () => {
-    const shortPassword = 'Pass1!';
-
-    const isValid = await yup
-      .string()
-      .required()
-      .test(
-        'has-digit',
-        'Your password must contain at least one digit',
-        (value) => /\d/.test(value),
-      )
-      .test(
-        'has-uppercase',
-        'Your password must contain at least one uppercase letter',
-        (value) => /[A-Z]/.test(value),
-      )
-      .test(
-        'has-lowercase',
-        'Your password must contain at least one lowercase letter',
-        (value) => /[a-z]/.test(value),
-      )
-      .test(
-        'has-special-char',
-        'Your password must contain at least one special character',
-        (value) => /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(value),
-      )
-      .min(8, 'Your password must be at least 8 characters long')
-      .isValid(shortPassword);
-
-    expect(isValid).toBe(false);
+  it('should accept a valid password', async () => {
+    await expect(passwordSchema.validate('Password1!')).resolves.toBe(
+      'Password1!',
+    );
   });
 });
